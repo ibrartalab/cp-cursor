616ef53eba2c569113682b7933d28d122d8c7a09
IbrarKhan <ibrartalabkhan@gmail.com>
2024-03-26 22:34:09 +0500

    create build

--- src/components/CursorMove.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CursorMove() {
-    return (
-        <div className="cursor-move">Moveing</div>
-    )
-}
-
-export default CursorMove
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-export * from "./components/CursorMove.jsx";
+export * from './components/OwnPointers.jsx';
