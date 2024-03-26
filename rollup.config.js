1609535ff12fb16ee71b1db3752ddf5b871239cc
IbrarKhan <ibrartalabkhan@gmail.com>
2024-03-26 16:24:25 +0500

    initial comment

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'bundle.js',
+		format: 'cjs'
+	},
+	plugins: [resolve(), babel({ babelHelpers: 'bundled' })]
+};
---
+++ src/components/CursorMove.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CursorMove() {
+    return (
+        <div className="cursor-move">Moveing</div>
+    )
+}
+
+export default CursorMove
